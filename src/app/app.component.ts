22e1188c652a79014eeee875dc9cce0d6af5ed15
Ridwan Nur <rdwnnr@yahoo.com>
2022-06-01 01:30:41 +0700

    Dagonmetric

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, Injectable } from '@angular/core';
+import LontaraRule from './lontara-rules.json';
+import { TranslitRuleItem, TranslitService } from '@dagonmetric/ng-translit';
+
+@Injectable()
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lat2lon translit';
+  latinInput!: string;
+  lontaraOutput!: string;
+
+  constructor(private readonly translitService: TranslitService) { }
+
+  onSubmit() {
+    const lat2lon: TranslitRuleItem[] = LontaraRule;
+
+    this.translitService.translit(this.latinInput.toLowerCase(), 'latin2lontara', lat2lon)
+    .subscribe( (result) => {
+      console.log(result);
+      this.lontaraOutput = result.outputText
+      .replace(/[^ᨀ-᨟0-9\s]/gi, '');
+      
+      console.log(this.lontaraOutput);
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TranslitModule } from '@dagonmetric/ng-translit';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    
+    // Dagonmetric
+    TranslitModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
